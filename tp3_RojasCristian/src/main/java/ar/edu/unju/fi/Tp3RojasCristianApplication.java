ea0ea82ed622160f624901656f8ba5f93d0904b8
cristianrojas07 <cristianrdev@gmail.com>
2024-05-06 18:17:29 -0300

    firstcommit

---
+++ tp3_RojasCristian/src/main/java/ar/edu/unju/fi/Tp3RojasCristianApplication.java
@@ -0,0 +1,14 @@
+package ar.edu.unju.fi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tp3RojasCristianApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tp3RojasCristianApplication.class, args);
+		System.out.println("test");
+	}
+
+}
